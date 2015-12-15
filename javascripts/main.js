17acab8ebb18a32cf172644460a11ecbf0565f0a
Lilia <ustimovalilit@gmail.com>
2015-12-15 12:23:22 +0200

    Изменила кнопки в шапке и название сайта.
    Signed-off-by: Lilia <ustimovalilit@gmail.com>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+$(function() {
+    var linkRelease = 'https://github.com/LiliaUst/trainIrregularVerbs/releases/download/{0}/trainIrregularVerbs.zip';
+    $.ajax({
+        url: 'params.json',
+        success: function(data) {
+            if (data && data.version) {
+                $('#download-zip').attr('href', linkRelease.replace('{0}', data.version));
+            }
+        }
+    });
+});
